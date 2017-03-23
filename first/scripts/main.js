8422602c36289003b9c0beec4eb7c4f0d513fd07
ethan <eevansnm2.0@gmail.com>
2017-03-23 11:59:41 -0600

    eight commit. added obstacle texture. freezes when generating in random location

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -12,6 +12,7 @@ function preload() {
     game.load.spritesheet('guy','../assets/test2.png', 32, 51);
     //loads the file 'test2.png' as a  spritesheat named 'guy' from directory '../assets/'
     //frames are specified as being 32 px wide and 51 px tall
+    game.load.image('obstacle','../assets/test3.png');
     
     };
 
@@ -46,14 +47,34 @@ function create() {
     };
 
 function update() {
-
+    
     if (player.world.x > game.world.width - 600)
     {
+        world_generation();
+    }
+    player_movement();
+    };
+
+function world_generation() {
+
     game.world.resize(game.world.width + 800, game.world.height);
     game.add.sprite(game.world.width - 800, 0, 'track');
-    player.bringToTop();
-    //brings the player to the top of the render order, so that it will appear above track
+
+    new_obstacle_count = Math.floor(Math.random() * (4)) + 1
+    while (new_obstacle_count > 0)
+    {
+        new_obstacle_location_y = Math.floor(Math.random() * (370 - 30 + 1)) + 30;
+        new_obstacle_location_x = Math.floor(Math.random() * (820 - 30 + 1)) + 30;
+        game.add.sprite(new_obstacle_location_x, new_obstacle_location_Y, 'obstacle');
+        game.add.sprite(100, 100, 'obstacle');
+        new_obstacle_count = new_obstacle_count - 1;
     }
+    //player.body.velocity.x = player.body.velocity + 1;
+    player.bringToTop();
+    //brings the player to the top of the render order
+    };
+
+function player_movement() {
 
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
