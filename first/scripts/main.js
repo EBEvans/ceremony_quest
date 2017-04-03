b029b808eba467494a65b4ffe31c7b06b98730b9
ethan <eevansnm2.0@gmail.com>
2017-04-03 11:55:33 -0600

    ninth commit. no real progress, began splitting obstacle generation and track generation

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -18,6 +18,7 @@ function preload() {
 
 var player;
 var cursors;
+var obstacles;
 
 function create() {
 
@@ -50,27 +51,23 @@ function update() {
     
     if (player.world.x > game.world.width - 600)
     {
-        world_generation();
+        track_generation();
+        obstacle_generation();
+        player.bringToTop();
     }
     player_movement();
     };
 
-function world_generation() {
+function obstacle_generation() {
+    
+    };
+
+function track_generation() {
 
     game.world.resize(game.world.width + 800, game.world.height);
     game.add.sprite(game.world.width - 800, 0, 'track');
 
-    new_obstacle_count = Math.floor(Math.random() * (4)) + 1
-    while (new_obstacle_count > 0)
-    {
-        new_obstacle_location_y = Math.floor(Math.random() * (370 - 30 + 1)) + 30;
-        new_obstacle_location_x = Math.floor(Math.random() * (820 - 30 + 1)) + 30;
-        game.add.sprite(new_obstacle_location_x, new_obstacle_location_Y, 'obstacle');
-        game.add.sprite(100, 100, 'obstacle');
-        new_obstacle_count = new_obstacle_count - 1;
-    }
-    //player.body.velocity.x = player.body.velocity + 1;
-    player.bringToTop();
+    //player.bringToTop();
     //brings the player to the top of the render order
     };
 
